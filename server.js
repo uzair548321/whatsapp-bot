f9ae947085ea58e73262945c74c95460bcd13029
dev global <cameroncambay6@gmail.com>
2026-03-14 17:32:25 +0530

    test simple whatsapp reply

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
+const express = require("express");
+const { MessagingResponse } = require("twilio").twiml;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+/* Root route */
+app.get("/", (req, res) => {
+  res.send("BOT VERSION TEST 14 MAR");
+});
+
+/* WhatsApp webhook */
 app.post("/whatsapp", (req, res) => {
   console.log("==== TEST WEBHOOK HIT ====");
   console.log("BODY:", req.body);
@@ -6,5 +20,11 @@ app.post("/whatsapp", (req, res) => {
   twiml.message("Bot reply test successful");
 
   res.set("Content-Type", "text/xml");
-  return res.status(200).send(twiml.toString());
+  res.status(200).send(twiml.toString());
+});
+
+const PORT = process.env.PORT || 10000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
 });
